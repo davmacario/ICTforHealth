e9e0e91fd0bb2203557baf92f9a711f15522a78b
Davide Macario <dav.macario@gmail.com>
2022-10-07 17:13:59 +0200

    Added lab0 folder and programs

---
+++ lab00/minimization_test.py
@@ -0,0 +1,11 @@
+import sub.minimization as mymin
+import numpy as np
+Np = 100
+Nf = 4
+A = np.random.randn(Np, Nf)
+w = np.random.randn(Nf,)
+y = A@w
+m = mymin.SolveLLS(y, A)
+m.run()
+m.print_result('LLS')
+m.plot_w_hat('LLS')
