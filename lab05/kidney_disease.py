83863b859b3fb78a72b10078e9cf04c5922b487c
Davide Macario <dav.macario@gmail.com>
2022-12-16 16:30:19 +0100

    Finished lab05

--- lab05/kidney_disease.py
+++ lab05/kidney_disease.py
@@ -1,8 +1,10 @@
+from sklearn.metrics import accuracy_score, confusion_matrix
 import pandas as pd
 import numpy as np
 from sklearn import tree
 import matplotlib.pyplot as plt
 import sklearn.ensemble as ske
+import seaborn as sn
 
 pd.set_option('display.precision', 3, 'display.max_columns',
               50, 'display.max_rows', 50)
@@ -58,11 +60,10 @@ xx = xx.replace(mapping.keys(), mapping.values())
 print(f"Cardinality of the features{xx.nunique()}")
 print(f"\nInformation about the data set: {xx.info()}")
 
-# %%
-# Manage the missing data through regression
+# %%  Manage the missing data through regression ################################
 x = xx.copy()
 
-# Drop rows with less than 19 = Nf-6 recorded features:
+# Drop rows with less (<=) than 19 = Nf-6 recorded features:
 min_n_values = Nf - 6
 x = x.dropna(thresh=min_n_values)
 # necessary to have index without "jumps"
@@ -93,7 +94,7 @@ mm = Xtrain.mean(axis=0)
 ss = Xtrain.std(axis=0)
 Xtrain_norm = (Xtrain-mm)/ss
 
-# %% REGRESSION
+# %% REGRESSION ################################################################
 # Get test dataset by removing rows with no null elements
 Xtest = x.drop(x[x.isnull().sum(axis=1) == 0].index)
 Xtest.reset_index(drop=True, inplace=True)  # reset the index of the dataframe
@@ -106,6 +107,7 @@ regr = tree.DecisionTreeRegressor()         # instantiate the regressor
 for kk in range(Np):
     xrow = Xtest_norm.iloc[kk]  # k-th row
     mask = xrow.isna()                        # columns with nan in row kk
+
     # remove the columns from the training dataset
     Data_tr_norm = Xtrain_norm.loc[:, ~mask]
     y_tr_norm = Xtrain_norm.loc[:, mask]       # columns to be regressed
@@ -116,16 +118,17 @@ for kk in range(Np):
     a[mask] = ytest_norm.flatten()
     # substitute nan with regressed values
     Xtest_norm.iloc[kk] = a
+
 Xtest_new = Xtest_norm*ss+mm                    # denormalize
 
-# %% substitute regressed numerical values with the closest element in the alphabet
+# %% Substitute regressed numerical values with the closest element in the alphabet
 # Determine where are the categorical features
 index = np.argwhere(feat_cat == 'cat').flatten()
 for k in index:
     val = alphabets[k]              # possible values for the feature
     c = Xtest_new.iloc[:, k].values  # values in the column
     c = c.reshape(-1, 1)            # column vector
-    val = val.reshape(1, -1)         # row vector
+    val = val.reshape(1, -1)        # row vector
     # matrix with all the distances w.r.t. the alphabet values
     d = (val-c)**2
     # find the index of the closest alphabet value
@@ -136,7 +139,7 @@ for k in index:
 X_new = pd.concat([Xtrain, Xtest_new], ignore_index=True, sort=False)
 X_new.to_csv('./data/kidney_no_nan.csv')
 
-# %% check the distributions
+# %% check the cumulative distribution functions
 L = X_new.shape[0]
 
 plotCDF = True
@@ -157,4 +160,71 @@ if plotCDF:
     plt.show()
 
 # %%------------------ Decision tree -------------------
-# add here the missing lines
+
+decision_tree = tree.DecisionTreeClassifier(criterion='entropy')
+random_forest = ske.RandomForestClassifier(
+    n_estimators=150, criterion='entropy')
+
+np.random.seed = 315054
+
+N_iter = 3
+
+acc_tree = []
+conf_tree = []
+
+acc_forest = []
+conf_forest = []
+
+for i in range(N_iter):
+    r = np.random.randint(100)
+    print('+------------------+')
+    print(f'| Random Seed: {r:3} |')
+    print('+------------------+')
+
+    Xsh = X_new.sample(frac=1, replace=False, random_state=r,
+                       axis=0, ignore_index=True)
+
+    Ntrain = L//2
+
+    XshTrain = Xsh[Xsh.index < Ntrain]
+    XshTest = Xsh[Xsh.index >= Ntrain]
+
+    y_tr = XshTrain['classk']
+    X_tr = XshTrain.drop('classk', axis=1)
+
+    decision_tree_fitted = decision_tree.fit(X_tr, y_tr)
+    y_hat_te = decision_tree_fitted.predict(XshTest.drop('classk', axis=1))
+
+    acc_tree.append(accuracy_score(XshTest['classk'], y_hat_te))
+    conf_tree.append(confusion_matrix(XshTest['classk'], y_hat_te))
+
+    print('Random Tree:')
+    print('Accuracy = ', acc_tree[i])
+    print(conf_tree[i])
+
+    target_names = ['notckd', 'ckd']
+
+    fig, axes = plt.subplots()
+    tree.plot_tree(decision_tree_fitted,
+                   feature_names=feat_names[:24],
+                   class_names=target_names,
+                   proportion=False,
+                   filled=True)
+
+    plt.title('shuffled_data (seed ' + str(r) + ')')
+    plt.savefig('./img/tree_shuffle_' + str(r) + '.png')
+    plt.show()
+
+    ##############################################################
+    # Random Forest Classifier
+
+    random_forest_fitted = random_forest.fit(X_tr, y_tr)
+
+    y_hat_te_for = random_forest_fitted.predict(XshTest.drop('classk', axis=1))
+
+    acc_forest.append(accuracy_score(XshTest['classk'], y_hat_te_for))
+    conf_forest.append(confusion_matrix(XshTest['classk'], y_hat_te_for))
+
+    print('Random Forest:')
+    print('Accuracy = ', acc_forest[i])
+    print(conf_forest[i])
