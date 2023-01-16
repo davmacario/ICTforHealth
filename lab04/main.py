c47211ac8557d960a4190bbee18f3104c3a04781
Davide Macario <dav.macario@gmail.com>
2023-01-16 18:34:13 +0100

    Update number of training slices + code tidiness

--- lab04/main.py
+++ lab04/main.py
@@ -47,13 +47,14 @@ sensors_IDs = list(range(n_sensors_tot))            # List of sensor IDs
 sensNamesSub = [sensNames[i] for i in sensors_IDs]  # Names of sensors
 
 # Number of training slices
-Nslices = 10
+Nslices = 11
 Ntot = 60                                # Total number of slices
 slices = list(range(1, Nslices+1))       # First Nslices to plot
 print(f"Training set slices: {Nslices}")
 
 fs = 25                                 # Hz, sampling frequency (fixed)
-samplesPerSlice = fs*5                  # Samples in each slice (fixed) - each slice is 5 seconds
+# Samples in each slice (fixed) - each slice is 5 seconds
+samplesPerSlice = fs*5
 
 #%%##########################################################################################
 # Features to be kept #############################################
@@ -70,7 +71,7 @@ with elements: (6, 15, 16, 24, 26, 33, 42, 43, 44)
 print('Number of used sensors: ', len(used_sensors))
 print("Used sensors: ", used_sensorNames)
 
-#### Splitting training and test sets
+# Splitting training and test sets
 n_slices_tr = Nslices
 n_slices_te = Ntot - n_slices_tr
 
@@ -93,12 +94,13 @@ Preprocessing pipeline:
 us_factor = 1
 n_elem_tr = round(n_elem_tr/us_factor)
 n_elem_te = round(n_elem_te/us_factor)
-preproc = Preprocessor(fs=fs, filt_type='bandstop', cutoff=[0.01, 8], us_factor=us_factor)
+preproc = Preprocessor(fs=fs, filt_type='bandstop', cutoff=[
+                       0.01, 8], us_factor=us_factor)
 
 preproc.plotFreqResp(saveimg=True)
 
-X_tr, y_tr, start_centroids, stdpoints = buildDataSet(filedir, patients, activities,\
-         slices_tr, used_sensors, preprocessor_obj=preproc, plots=True, savefig=True)
+X_tr, y_tr, start_centroids, stdpoints = buildDataSet(filedir, patients, activities,
+                                                      slices_tr, used_sensors, preprocessor_obj=preproc, plots=True, savefig=True)
 
 
 #%%##########################################################################################
@@ -109,13 +111,15 @@ interCentroidDist(start_centroids, actNamesShort, plot=True, save_img=True)
 #%%##########################################################################################
 # Find minimum distance between each centroid and all others
 
-centroidSeparationPlot(start_centroids, stdpoints, actNamesShort, save_img=True)
+centroidSeparationPlot(start_centroids, stdpoints,
+                       actNamesShort, save_img=True)
 
 #%%##########################################################################################
 # K-MEANS ################################
 
 # Initialize centroids as the mean centroids evaluated on the training set
-k_means = KMeans(n_clusters=n_clusters, init=start_centroids, n_init=1, max_iter=1000, tol=1e-10)
+k_means = KMeans(n_clusters=n_clusters, init=start_centroids,
+                 n_init=1, max_iter=1000, tol=1e-10)
 k_means_fitted = k_means.fit(X_tr)
 
 # Classes id's are between 1 and 19 (not 0 and 18)
@@ -124,17 +128,18 @@ k_means_fitted = k_means.fit(X_tr)
 # Associate to each label the correct activity
 mapping_ind = np.zeros((n_clusters,), dtype=np.int8)
 
-# Approach: each centroid corresponds to the class of the 
-# closest 'centroid' found before as the mean 
+# Approach: each centroid corresponds to the class of the
+# closest 'centroid' found before as the mean
 # (IT WORKS! - Notice: average centroids were also used to initialize K-means centroids)
 for i in range(n_clusters):
     centr_curr = k_means_fitted.cluster_centers_[i, :]
     dist_cent = dist_eval(centr_curr, start_centroids)
     mapping_ind[i] = np.argmin(dist_cent)+1
-    
+
     plt.figure()
     plt.plot(centr_curr, label="centroid "+str(i))
-    plt.plot(start_centroids[mapping_ind[i]-1, :], ":", label="closest element")
+    plt.plot(start_centroids[mapping_ind[i]-1, :],
+             ":", label="closest element")
     plt.grid()
     plt.legend()
     plt.xticks(np.arange(X_tr.shape[1]), list(used_sensorNames), rotation=90)
@@ -147,7 +152,8 @@ plt.show()
 
 plt.figure()
 for i in range(n_clusters):
-    lines = plt.plot(k_means_fitted.cluster_centers_[i, :], label=str(mapping_ind[i]))
+    lines = plt.plot(k_means_fitted.cluster_centers_[
+                     i, :], label=str(mapping_ind[i]))
     lines[0].set_color(cm(i//3*3/NAc))
     lines[0].set_linestyle(line_styles[i % 3])
 plt.grid()
@@ -158,11 +164,12 @@ plt.tight_layout()
 plt.savefig('./img/centroids.png')
 plt.show()
 
-### Test set
+# Test set
 
-X_te, y_te = buildDataSet(filedir, patients, activities, slices_te, used_sensors, preprocessor_obj=preproc, plots=False)[:2]
+X_te, y_te = buildDataSet(filedir, patients, activities, slices_te,
+                          used_sensors, preprocessor_obj=preproc, plots=False)[:2]
 
-################ Performance analysis
+# Performance analysis
 
 y_hat_tr = k_means_fitted.predict(X_tr)
 y_hat_te = k_means_fitted.predict(X_te)
@@ -170,11 +177,13 @@ y_hat_te = k_means_fitted.predict(X_te)
 # Accuracy
 acc_tr_kmeans = evalAccuracy(y_hat_tr+1, y_tr)
 acc_te_kmeans = evalAccuracy(y_hat_te+1, y_te)
-#### Notice: the label saved in y_hat goes from 0 to 18, not from 1 to 19
+# Notice: the label saved in y_hat goes from 0 to 18, not from 1 to 19
 
 # Confusion matrix
-cm_tr = plotConfusionMatrix(y_hat_tr+1, y_tr, actNamesShort, title='Confusion Matrix, train', save_img=True, img_path='img/conf_mat_tr.png')
-cm_te = plotConfusionMatrix(y_hat_te+1, y_te, actNamesShort, title='Confusion Matrix, test', save_img=True, img_path='img/conf_mat_te.png')
+cm_tr = plotConfusionMatrix(y_hat_tr+1, y_tr, actNamesShort,
+                            title='Confusion Matrix, train', save_img=True, img_path='img/conf_mat_tr.png')
+cm_te = plotConfusionMatrix(y_hat_te+1, y_te, actNamesShort,
+                            title='Confusion Matrix, test', save_img=True, img_path='img/conf_mat_te.png')
 
 print(f"Accuracy (training): {acc_tr_kmeans}")
 print(f"Accuracy (test): {acc_te_kmeans}")
@@ -183,9 +192,11 @@ print(f"Accuracy (test): {acc_te_kmeans}")
 acc_act_tr = evalAccuracyClasses(y_hat_tr + 1, y_tr)
 acc_act_te = evalAccuracyClasses(y_hat_te + 1, y_te)
 
-acc_class_nd = np.stack([acc_act_tr.reshape((NAc,)), acc_act_te.reshape((NAc,))], axis=0)
+acc_class_nd = np.stack(
+    [acc_act_tr.reshape((NAc,)), acc_act_te.reshape((NAc,))], axis=0)
 
-acc_classes = pd.DataFrame(acc_class_nd, columns=actNamesShort, index=['Accuracy, train', 'Accuracy, test'])
+acc_classes = pd.DataFrame(acc_class_nd, columns=actNamesShort, index=[
+                           'Accuracy, train', 'Accuracy, test'])
 
 pd.set_option('display.max_rows', 6)
 pd.set_option('display.max_columns', 20)
